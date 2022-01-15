79ec7aacc73bd3e6b141e76baacf7885bb7e5fdb
SimoneFolino91 <47349597+SimoneFolino91@users.noreply.github.com>
2022-01-15 15:43:51 +0100

    Update WordCount in PysparkSQL.py

--- WordCount in PysparkSQL.py
+++ WordCount in PysparkSQL.py
@@ -16,4 +16,21 @@ display(df.select(F.explode(F.split(F.regexp_replace('phrase', '\n', ' '),' ')).
        )
 
 
-#### Using the 
+#### Using the Array Function
+def filter_out_empty_string(column):
+  return F.filter(column, lambda x: x != '') # Remember the element value value of an array is a column object!!!!
+
+def lower_case_for_element_in_array(column):
+  return F.transform(column, lambda x: F.lower(x)) # Remember the element value of an array is a column object!!!!
+
+display(df.select(F.explode(lower_case_for_element_in_array(filter_out_empty_string(F.split(F.regexp_replace('phrase', '\n', ' '),' ')))).alias('words'))
+        .groupby('words')
+         .count().orderBy(F.col('count'), ascending = False)
+       )
+"""
+Comments: 
+1) Replace the \n with " " with F.regex_replace
+2) Split the words into an array 
+3) Filter out using the function filter_out_empty_string (No UDF) the " " char from the array
+4) Map the words of the array into a 
+"""
